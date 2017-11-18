097e1c2ddfcfae037f40999937f2aec10597eb63
crosanom <crosanom@192.168.1.43>
2017-11-18 19:58:42 +0100

    Commit 11.1 : Completa al anterior pero se soluciona un problema de sintaxis de java, se recoloca la llave de cierre de la clase que se había borrado por error. Y corrigiendo la indentación.

--- Rosano_Morin_PROG_Tarea03/src/tarea03/Rey.java
+++ Rosano_Morin_PROG_Tarea03/src/tarea03/Rey.java
@@ -16,8 +16,41 @@ public class Rey {
 	Color color;
 	Posicion posicion;
 
-	public Rey() {
+	/**
+	 * @param color
+	 */
+	public Rey(Color color) { // sobre carga de constructor pasando por
+								// parametro color
+
+		String mensajeRBlanco = "El rey blanco esta en fila1 columna e";
+		String mensajeRNegro = "El rey blanco esta en fila8 columna e";
+
+		// validamos la entrada de filas con un if-else
+
+		if (color == Color.BLANCO) {
+			this.posicion.setColumna('e');
+			this.posicion = this.posicion;
+			System.out.println(mensajeRBlanco);
+
+			this.posicion.setFila(1);
+			this.posicion = this.posicion;
+
+			if (color == Color.NEGRO) {
+
+				this.posicion.setColumna('e');
+				this.posicion = this.posicion;
+
+				this.posicion.setFila(8);
+				this.posicion = this.posicion;
+				System.out.println(mensajeRNegro);
+			}else{
+				System.out.println("posicion introducida incorrecta, revisar posición ");
+		}
 
 	}
+
+	// if (color==color.NEGRO)
+
+	// validamos la entrada de columna
 
 }
