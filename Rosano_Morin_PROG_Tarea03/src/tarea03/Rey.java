3cb6c89096130e41599c40e076d2b1666f1cf205
crosanom <crosanom@192.168.1.43>
2017-11-18 19:53:12 +0100

    Commit 11 : Crear un constructor para la clase anterior que acepte como parámetros el color. Se hace  if valide la posicion para cada uno de los reyes  rey blanco se posiciona inicialmente en la casilla e1 y el negro en la casilla e8.

--- Rosano_Morin_PROG_Tarea03/src/tarea03/Rey.java
+++ Rosano_Morin_PROG_Tarea03/src/tarea03/Rey.java
@@ -4,16 +4,20 @@
 package tarea03;
 
 /**
- * @author crosanom
- *Clase rey con dos atributos color y posicion 
+ * @author crosanom Clase rey con dos atributos color y posicion
  */
+
 public class Rey {
-	
+
 	/**
-	 * 
+	 * atributos de la clase rey son: color y posición con un modificador
+	 * default acceso a cualquier clase dentro del paquete tarea 03
 	 */
+	Color color;
+	Posicion posicion;
+
 	public Rey() {
-		// TODO Auto-generated constructor stub
+
 	}
 
 }
