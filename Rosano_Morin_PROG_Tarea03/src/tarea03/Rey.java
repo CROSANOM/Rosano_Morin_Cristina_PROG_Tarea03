e38330cf30afde9f2cb1920cd32c1fa74a6e7ad8
crosanom <crosanom@192.168.1.43>
2017-11-18 19:16:13 +0100

    Commit 10: Crear la clase Rey, dentro del paquete anterior, cuyos atributos serán un color (del tipo enumerado Color) y posicion (de la clase Posicion), con la visibilidad adecuada. No se usa modificar de acceso, por tanto el acceso es defaulto  por defecto, ya los atributos que va a usar estan en el mismo paquete. Por tanto  la clase Rey  accede a los atributos de Color enum, y Posición.

---
+++ Rosano_Morin_PROG_Tarea03/src/tarea03/Rey.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package tarea03;
+
+/**
+ * @author crosanom
+ *Clase rey con dos atributos color y posicion 
+ */
+public class Rey {
+	
+	/**
+	 * 
+	 */
+	public Rey() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
