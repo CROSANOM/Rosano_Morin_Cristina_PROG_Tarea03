a59e6cb84b807392106656327a4ca0d2c9c546ae
crosanom <crosanom@192.168.1.43>
2017-11-20 23:21:19 +0100

    Commit : clase Principal crear un rey de un color pedido por teclado. Usar el metodo mover creado en la clase rey

--- Rosano_Morin_PROG_Tarea03/src/tarea03/Principal.java
+++ Rosano_Morin_PROG_Tarea03/src/tarea03/Principal.java
@@ -1,14 +1,63 @@
 package tarea03;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import utilidades.Entrada;
+
 public class Principal {
 
-	public Principal() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+		String colorRey;
+
+		/*
+		 * validar que el usuario introduce correctamente el color ahora mismo
+		 * no me funciona la validación do {
+		 * 
+		 * System.out.println("INTRODUCE color "); color = Entrada.cadena();
+		 * 
+		 * // Mensaje recordartorio de entrada. if (color !="blanco" ||
+		 * color!="negro") {
+		 * System.out.println("error rey solo puede ser Blanco o Negro"); }
+		 * 
+		 * } while (color !="blanco" || color!="negro");
+		 */
+
+
+		
+		System.out.println("Dame un color para el rey ");
+		colorRey = Entrada.cadena();
+		// String colorIncorrecto;
+
+		Pattern pat = Pattern.compile("(blanco|Negro)+");
+		Matcher mat = pat.matcher(colorRey);
+		if (mat.matches()) {
+			System.out.println("COLOR ES CORRECTO");
+
+		
+		} else { // deberia de haber usado un do while pero no daba con la
+					// tecla.
+			System.out.println("COLOR ES INCORRECTO");
+			System.out.println("RECUERDA QUE el color del Rey puede ser blanco o negro");
+			System.out.println("Dame un color para el rey");
+			colorRey = Entrada.cadena();
+		}
+
+		
+		// Crear un rey usando el constructor de la clase Rey
+		
+		System.out.println("comprobar en que parte del codigo se sale");
+		
+		Rey miFiguraRey =new Rey ();
+		miFiguraRey.getColor();
+		System.out.println("el rey es"+miFiguraRey.color);
+		
+
+		
+		
+		
+
+	}// main
 
 }
+// fin class
