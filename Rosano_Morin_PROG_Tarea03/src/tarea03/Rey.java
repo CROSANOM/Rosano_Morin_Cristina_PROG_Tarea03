dda193d061645e43e6c6e909b58b64171d528fc9
crosanom <crosanom@192.168.1.43>
2017-11-20 21:05:32 +0100

    Commit : Se incluyen modificaciones hechas en Clase Rey: - Incoporar un mensaje que indica la posicion del rey blanco y otro para la posición del rey negro al instanciarlos a través  el constructor pasándole como parametro color. Tal y como se solicita  en el   13 de la tarea. - Se corrige el punto 14 de la tarea, aprovechando el constructor por defecto incluyendo los atributos de un reyBlanco posicion,color correspondiente haciendo uso de los metodos get y set de la clase Posición. - Eliminado los setters de los atributos ya que no se solicitan ni se van  a emplear en este clase. - Eliminado los metodos setter y getter del objeto reyBlanco, que había anteriormente. -Corrige indentación

--- Rosano_Morin_PROG_Tarea03/src/tarea03/Rey.java
+++ Rosano_Morin_PROG_Tarea03/src/tarea03/Rey.java
@@ -2,6 +2,7 @@
  * 
  */
 package tarea03;
+
 /**
  * @author crosanom
  * @version 1.1 Clase rey con dos atributos color y posicion
@@ -10,8 +11,8 @@ package tarea03;
 public class Rey {
 
 	/*
-	 * Atributos de la clase rey son: color y posición  default
-	 * acceso a cualquier clase dentro del paquete tarea
+	 * Atributos de la clase rey son: color y posición default acceso a
+	 * cualquier clase dentro del paquete tarea
 	 */
 
 	/**
@@ -19,13 +20,11 @@ public class Rey {
 	 */
 	Color color;
 	Posicion posicion;
-	
-	//Tablero ajedrez
-	//static int filaTablero [ ] = {1,2,3,4,5,6,7,8};
-	char columnaTablero [ ] = {'a','b','c','d','e','f','g','h'};
-	
-	
-	/** 
+
+	// Tablero ajedrez
+	char columnaTablero[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' };
+
+	/**
 	 * Constructor por defecto.
 	 * 
 	 */
@@ -34,14 +33,15 @@ public class Rey {
 		this.posicion.setColumna('e');
 		this.color = color.BLANCO;
 	}
-	
-	
-	/**constructor Rey que se le pasa 
+
+	/**
+	 * constructor Rey que se le pasa
+	 * 
 	 * @param color
 	 * 
 	 */
 
-	public Rey (Color color) {
+	public Rey(Color color) {
 
 		String mensajeRBlanco = "La posición del rey Blanco es fila 1 columna e";
 		String mensajeRNegro = "La posición del rey Negro es fila 8 columana e";
@@ -50,36 +50,33 @@ public class Rey {
 			this.posicion.setFila(1);
 			this.posicion.setColumna('e');
 		}
-			
+
 		if (color == color.NEGRO) {
 			this.posicion.setFila(8);
 			this.posicion.setColumna('e');
-		} 
+		}
 
-	}	
-	
+	}
 
-	//Getters  de los atributos: Color, Posicion.
-	
+	// Getters de los atributos: Color, Posicion.
 
-	
 	/**
-	 * get color 
+	 * get color
+	 * 
 	 * @return the color
 	 */
 	protected Color getColor() {
 		return color;
 	}
 
-
 	/**
 	 * get posicion
+	 * 
 	 * @return the posicion
 	 */
 	protected Posicion getPosicion() {
 		return posicion;
 	}
-
 
 	/**
 	 * Metodo toString usa con IDE
@@ -89,54 +86,75 @@ public class Rey {
 		return "Rey [color =" + color + ", posicion=" + posicion + "]";
 	}
 
-	/**metodo mueve modifica Direccion pasa
+	/**
+	 * metodo mueve modifica Direccion pasa
+	 * 
 	 * @param direccion
 	 */
-	public void mueve (Direccion direccion) {
+	public void mueve(Direccion direccion) {
 
 		// Comprobar si puede moverse en el tablero.
 		// Cuando es norte.
 		if (direccion == direccion.NORTE) {
-			
-			//Saber donde estamos.
-			if (posicion.getFila()>1) {
-				posicion.setFila(posicion.getFila()-1);	
+
+			// Saber donde estamos.
+			if (posicion.getFila() > 1) {
+				posicion.setFila(posicion.getFila() - 1);
 			} else {
 				System.out.println("No es posible realizar ese movimiento.");
-			}	
+			}
 		}
-			
+
 		if (direccion == direccion.SUR) {
-			
-			//Saber donde estamos.
-			if (posicion.getFila()<8) {
-				posicion.setFila(posicion.getFila()+1);	
+
+			// Saber donde estamos.
+			if (posicion.getFila() < 8) {
+				posicion.setFila(posicion.getFila() + 1);
 			} else {
 				System.out.println("No es posible realizar ese movimiento.");
-			}	
-		}	
-			
+			}
+		}
+
 		// Cuando es Oeste.
 		if (direccion == direccion.OESTE) {
-			
-			//Saber donde estamos en el tablero para las columnas.
-			for (int i=0 ; i<8 ; i++) {
-				
+
+			// Saber donde estamos en el tablero para las columnas.
+			for (int i = 0; i < columnaTablero.length; i++) {
+
 				// Si encuentra la letra en el tablero.
 				if (posicion.getColumna() == columnaTablero[i]) {
-					// Si es true, ya lo hemos encontrado, comprobar si puede moverse.	
-					if (i>1) {
-						posicion.setColumna(columnaTablero[i-1]);
-						
+					// Si es true, ya lo hemos encontrado, comprobar si puede
+					// moverse.
+					if (i > 1) {
+						posicion.setColumna(columnaTablero[i - 1]);
+
 					} else {
 						System.out.println("No es posible realizar ese movimiento.");
 					}
-				}	
-			
+				}
+			}
+
+			// Cuando es Este.
+			if (direccion == direccion.ESTE) {
+
+				// Saber donde estamos en el tablero para las columnas.
+				for (int i = 0; i < columnaTablero.length; i++) {
+
+					// Si encuentra la letra en el tablero.
+					if (posicion.getColumna() == columnaTablero[i]) {
+						// Si es true, ya lo hemos encontrado, comprobar si
+						// puede moverse.
+						if (i < 8) {
+							// Se actualiza posicion.
+							posicion.setColumna(columnaTablero[i + 1]);
+
+						} else {
+							System.out.println("No es posible realizar ese movimiento.");
+						}
+					}
+				}
+			}
+
 		}
-		
-		
-
 	}
-
 }
